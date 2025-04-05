666fa988a375b2ab0928a6bafbe2027a4e429f5d
Pascal-Juma <pascaljuma2002@gmail.com>
2025-04-05 21:41:30 +0300

    Build the Left Card component and some code refacturing

--- src/shared-theme/AppTheme.tsx
+++ src/shared-theme/AppTheme.tsx
@@ -1,4 +1,3 @@
-
 import * as React from 'react';
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import type { ThemeOptions } from '@mui/material/styles';
@@ -11,7 +10,6 @@ import { colorSchemes, typography, shadows, shape } from './themePrimitives';
 
 interface AppThemeProps {
   children: React.ReactNode;
-
   disableCustomTheme?: boolean;
   themeComponents?: ThemeOptions['components'];
 }
@@ -22,11 +20,12 @@ export default function AppTheme(props: AppThemeProps) {
     return disableCustomTheme
       ? {}
       : createTheme({
+          // For more details about CSS variables configuration, see https://mui.com/material-ui/customization/css-theme-variables/configuration/
           cssVariables: {
             colorSchemeSelector: 'data-mui-color-scheme',
             cssVarPrefix: 'template',
           },
-          colorSchemes, 
+          colorSchemes, // Recently added in v6 for building light & dark mode app, see https://mui.com/material-ui/customization/palette/#color-schemes
           typography,
           shadows,
           shape,
--- src/sign-up/SignUp.tsx
+++ src/sign-up/SignUp.tsx
@@ -1,19 +1,19 @@
 import * as React from "react";
-import {
-  Box,
-  Divider,
-  Button,
-  Checkbox,
-  CssBaseline,
-  FormControlLabel,
-  FormLabel,
-  FormControl,
-  Link,
-  TextField,
-  Typography,
-  Stack,
-} from "@mui/material";
+import Box from '@mui/material/Box';
+import Button from '@mui/material/Button';
+import Checkbox from '@mui/material/Checkbox';
+import CssBaseline from '@mui/material/CssBaseline';
+import Divider from '@mui/material/Divider';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormLabel from '@mui/material/FormLabel';
+import FormControl from '@mui/material/FormControl';
+import Link from '@mui/material/Link';
+import TextField from '@mui/material/TextField';
+import Typography from '@mui/material/Typography';
+import Stack from '@mui/material/Stack';
 import MuiCard from "@mui/material/Card";
+import CardContent  from "@mui/material/CardContent";
+import  CardMedia from "@mui/material/CardMedia";
 import { styled } from "@mui/material/styles";
 import AppTheme from "../shared-theme/AppTheme";
 import ColorModeSelect from "../shared-theme/ColorModeSelect";
@@ -22,6 +22,7 @@ import {
   FacebookIcon,
   SitemarkIcon,
 } from "./components/CustomIcons";
+import musicImage from "../assets/music.jpeg"
 
 const Card = styled(MuiCard)(({ theme }) => ({
   display: "flex",
@@ -30,11 +31,11 @@ const Card = styled(MuiCard)(({ theme }) => ({
   width: "100%",
   padding: theme.spacing(4),
   gap: theme.spacing(2),
-  margin: "auto",
+  margin: 0,
   boxShadow:
     "hsla(220, 30%, 5%, 0.05) 0px 5px 15px 0px, hsla(220, 25%, 10%, 0.05) 0px 15px 35px -5px",
   [theme.breakpoints.up("sm")]: {
-    width: "450px",
+    width: "400px",
   },
   ...theme.applyStyles("dark", {
     boxShadow:
@@ -44,10 +45,10 @@ const Card = styled(MuiCard)(({ theme }) => ({
 
 const SignUpContainer = styled(Stack)(({ theme }) => ({
   minHeight: "150dvh",
+  position: "relative",
   padding: theme.spacing(2),
   [theme.breakpoints.up("sm")]: {
     padding: theme.spacing(4),
-    position: "relative",
   },
   "&::before": {
     content: '""',
@@ -129,8 +130,23 @@ export default function SignUp(props: { disableCustomTheme?: boolean }) {
     <AppTheme {...props}>
       <CssBaseline enableColorScheme />
       <ColorModeSelect sx={{ position: "fixed", top: "1rem", right: "1rem" }} />
-      <SignUpContainer direction="column" justifyContent="space-between">
-        <Card variant="outlined">
+      <SignUpContainer direction={{ xl: "row", lg: "row", md: "column" }} sx={{ alignItems: "center", justifyContent: "center"}}>
+      <Card sx={{ width: 400, height: 706, display: "flex", padding: 0, gap: 0, borderTopRightRadius: 0, borderBottomRightRadius: 0, borderRight: "none"}}>
+      <CardContent sx={{  padding: 2}}>
+          <Typography variant="h5" >
+            Create your Account and start 
+            Listening to your Top Musics FOR FREE
+          </Typography>
+        </CardContent>
+        <CardMedia 
+        component ="img"
+        width = "450"
+        image = {musicImage}
+        alt = "music"
+        sx={{ height: "100%", width: "100%", objectFit: "cover"}}
+        />
+      </Card>
+        <Card variant="outlined" sx={{ borderTopLeftRadius: 0, borderBottomLeftRadius: 0, borderLeft: "none"}}>
           <SitemarkIcon />
           <Typography
             component="h1"
@@ -191,7 +207,7 @@ export default function SignUp(props: { disableCustomTheme?: boolean }) {
             </FormControl>
             <FormControlLabel
               control={<Checkbox value="allowExtraEmails" color="primary" />}
-              label="I want to receive updates via email."
+              label="Accept Terms & Conditions and Privary Policy"
             />
             <Button
               type="submit"
